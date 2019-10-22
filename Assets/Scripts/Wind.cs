0f9c070cce567c87a3eb1d8a02dc0effaf547ce5
Mmarcopollo <szafranmarek97@gmail.com>
2019-10-22 22:32:14 +0200

    Add wind but doesn't work XD

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+ 
+     void Start()
+    {
+        var tempRotation = Quaternion.identity;
+        var tempVector = Vector3.zero;
+        tempVector = tempRotation.eulerAngles;
+        tempVector.y = Random.Range(0, 359);
+        tempRotation.eulerAngles = tempVector;
+        transform.rotation = tempRotation;
+    }
+
+}
+
