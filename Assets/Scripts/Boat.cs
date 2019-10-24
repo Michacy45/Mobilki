9675a45d022d3f3b91312e874db9bea8d4299c7f
Mmarcopollo <szafranmarek97@gmail.com>
2019-10-24 12:32:23 +0200

    Add counting

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boat : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+
+    private GameObject player;
+    private Rigidbody rigidbody;
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCount();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speed*Input.GetAxis("Vertical") * Time.deltaTime*-1, 0f, speed* Input.GetAxis("Horizontal") * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCount();
+        }
+    }
+
+    void SetCount()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
