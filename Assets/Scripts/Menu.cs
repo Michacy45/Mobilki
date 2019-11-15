9f799bd0a3a72d757064ca8d2cd78982bca5cf74
Mmarcopollo <szafranmarek97@gmail.com>
2019-11-15 19:29:47 +0100

    Main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button startBtn;
+    public Button exitBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartGame()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        Application.Quit();
+    }
+}
