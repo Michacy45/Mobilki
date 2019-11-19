436c7da15139645ce840bcc91ec2f430ff8581c0
Michacy45 <pes.mich@gmail.com>
2019-11-19 23:00:15 +0100

    buildowanie na telefon

--- Assets/Scripts/RudderControll.cs
+++ Assets/Scripts/RudderControll.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class RudderControll : MonoBehaviour
 {
     private int direction;
+    private float widthScreen;
     // Start is called before the first frame update
     void Start()
     {
         direction = 0;
+        widthScreen = Screen.width;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.E))
+        /*if (Input.GetKey(KeyCode.E))
         {
             direction = -1;
         }
@@ -26,6 +28,24 @@ public class RudderControll : MonoBehaviour
         {
             direction = 0;
         }
-        transform.Rotate(0, Time.deltaTime * 100 * direction, 0);
+        transform.Rotate(0, Time.deltaTime * 100 * direction, 0);*/
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).position.x > widthScreen/2)
+            {
+                direction = -1;
+            }
+            else if (Input.GetTouch(i).position.x < widthScreen / 2)
+            {
+                direction = 1;
+            }
+            else
+            {
+                direction = 0;
+            }
+            transform.Rotate(0, Time.deltaTime * 100 * direction, 0);
+            i++;
+        }
     }
 }
